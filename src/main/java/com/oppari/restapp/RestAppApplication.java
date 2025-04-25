726ba34ad4394d985449d4215ea7e1b72f0c3ddb
titunen <titunen@grgdjh.com>
2025-04-25 20:30:24 +0300

    uusi

--- src/main/java/com/oppari/restapp/RestAppApplication.java
+++ src/main/java/com/oppari/restapp/RestAppApplication.java
@@ -8,7 +8,7 @@ public class RestAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestAppApplication.class, args);
-		System.out.println("Kesä on kreisii, ja grillataan");
+		System.out.println("Testiksi testiä");
 	}
 
 }
