4d4dfc9d7476a13c7bd4fb1af465d75b251f0499
titunen <titunen@grgdjh.com>
2025-04-26 11:48:19 +0300

    jotainffg

--- src/main/java/com/oppari/restapp/RestAppApplication.java
+++ src/main/java/com/oppari/restapp/RestAppApplication.java
@@ -8,7 +8,7 @@ public class RestAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestAppApplication.class, args);
-		System.out.println("Testiksi testi ja taas");
+		System.out.println("Testiksi testi ja taas ja taas");
 	}
 
 }
