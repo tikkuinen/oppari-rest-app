51d75245040e04263fd9ce529937b94b206d5f4e
titunen <titunen@grgdjh.com>
2025-04-27 22:11:16 +0300

    kissa testiksi

--- src/main/java/com/oppari/restapp/RestAppApplication.java
+++ src/main/java/com/oppari/restapp/RestAppApplication.java
@@ -8,7 +8,7 @@ public class RestAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestAppApplication.class, args);
-		System.out.println("Testiksi muutoksia miau ja vuh ja mur ja röh");
+		System.out.println("kissa sanoo miau");
 	}
 
 }
